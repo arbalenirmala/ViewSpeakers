b25dffe4fea2df031dd60dbf493b8df3356a6200
Nirmala <n.kudal@wawibox.de>
2022-12-12 03:21:55 +0530

    service

---
+++ src/app/service/speaker-list.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpeakerListService {
+
+  configUrl="https://randomuser.me/api";
+  constructor(private http: HttpClient) { }
+
+  getSpeakersList(res:number, pageNum:number) {
+    return this.http.get(this.configUrl + '/?results='+res+'&page='+pageNum);
+  }
+}
