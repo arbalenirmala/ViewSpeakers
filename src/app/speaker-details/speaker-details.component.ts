cb57cdfddb3da977c5e64d1ffd3baa0dfadf800d
Nirmala <n.kudal@wawibox.de>
2022-12-12 03:20:57 +0530

    speaker details

---
+++ src/app/speaker-details/speaker-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {SharedService} from '../service/shared.service';
+import { Router  } from '@angular/router';
+
+@Component({
+  selector: 'app-speaker-details',
+  templateUrl: './speaker-details.component.html',
+  styleUrls: ['./speaker-details.component.css']
+})
+export class SpeakerDetailsComponent implements OnInit {
+  public spData:any;
+  constructor(private sharedService :SharedService , private _router: Router) { }
+
+  ngOnInit(): void {
+    this.spData = this.sharedService.spObject;
+  }
+
+  backToPage(){
+    this._router.navigate(['../speakerInfo']);
+  }
+
+}
