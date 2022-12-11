c2c713207d84443d9f58712f6189dcdee721bf24
Nirmala <n.kudal@wawibox.de>
2022-12-12 03:22:33 +0530

    all files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SpeakerDetailsComponent} from './speaker-details/speaker-details.component';
+import {SpeakersListComponent} from './speakers-list/speakers-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'details',
+    component: SpeakerDetailsComponent,
+    title: 'Speaker Details',
+  },
+  {path: '', redirectTo: 'speakerInfo', pathMatch: 'full'},
+  {path: 'speakerInfo', component: SpeakersListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SpeakersListComponent } from './speakers-list/speakers-list.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NgxPaginationModule} from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { FormsModule } from '@angular/forms';
+import { SearchPipe} from './search.pipe';
+import { SpeakerDetailsComponent } from './speaker-details/speaker-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpeakersListComponent,
+    FooterComponent,
+    HeaderComponent,
+    SearchPipe,
+    SpeakerDetailsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
